6b6bfd72ca2feddf98aa8c9dcc9466461befa894
Esteban Andres Murcia Acuña <estebandres@proton.me>
2023-09-01 18:43:28 -0500

    Added the add,get,getFirst,getLast,remove,removeAll,isEmpty and growArray functions

--- src/CArrayList.java
+++ src/CArrayList.java
@@ -1,16 +1,74 @@
 import java.util.Arrays;
 import java.util.Iterator;
-
+/**
+   This class defines a simple implementation of the ArrayList class (Custom ArrayList)
+ **/
 public class CArrayList implements Iterable{
+    // Class attributes
     private int list[];
     private int size;
+    private int highestIndex;
     public CArrayList(){
-        this.list  = new int[10];
+        this.list  = new int[0];
         this.size = list.length;
+        highestIndex = 0;
     }
 
+    /**
+        @return Returns the current Size of the internal list of the implementation
+     **/
     public int size(){
         return size;
+    }
+
+    public void add(int value){
+        if(size<=highestIndex){
+            growArray();
+            size = list.length;
+        }
+        list[highestIndex++]=value;
+    }
+
+    public void add(int index, int value){
+        list[index]=value;
+    }
+
+    public void addAll(int... values){
+        for(int value: values){
+            add(value);
+        }
+    }
+    public int get(int index) {
+        return list[index];
+    }
+    public int getFirst(){
+        return list[0];
+    }
+    public int getLast(){
+        return list[highestIndex-1];
+    }
+    public void remove(){
+        list[--highestIndex]=0;
+    }
+
+    public void remove(int index){
+        int[] firstSplitList = new int[index];
+        int[] secondSplitList = new int[size-index];
+        list[index]=0;
+    }
+
+    public void removeAll(){
+        list = new int[0];
+    }
+    public boolean isEmpty(){
+        return size==0;
+    }
+    private void growArray(){
+        int newSize[] = new int[size+size/2+1];
+        for(int i=0; i<list.length;i++){
+            newSize[i]=list[i];
+        }
+        list = newSize;
     }
     @Override
     public Iterator iterator() {
@@ -19,8 +77,8 @@ public class CArrayList implements Iterable{
 
     @Override
     public String toString() {
-        return "CArrayList{" +
-                Arrays.toString(list) +
-                '}';
+        return
+                Arrays.toString(list)
+                ;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,30 @@
+/**
+@author Esteban Andres Murcia Acuña
+ @version: 09/01/2023
+**/
 import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
         CArrayList list = new CArrayList();
-        ArrayList<Integer> a = new ArrayList<>();
         System.out.println(list.size());
+        System.out.println(list.toString());
+        System.out.println(list.isEmpty());
+        for(int i=1;i<15;i++) {
+            list.add(i);
+            System.out.println(list.toString());
+            System.out.println(list.size());
+            System.out.println(list.isEmpty());
+        }
+        list.add(0,23);
+        System.out.println(list.getFirst());
+        System.out.println(list.getLast());
+        list.remove();
+
+        System.out.println(list.toString());
+        list.remove(3);
+        System.out.println(list.toString());
+        list.removeAll();
         System.out.println(list.toString());
     }
 }
